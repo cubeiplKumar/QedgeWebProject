c77fe07c1cca2ba4dd2cc48d911ed3f9ba6e8a05
cubeiplkumar <emailanilkumarm@gmail.com>
2025-03-22 09:21:52 +0530

    feature202 changes

--- Application.java
+++ Application.java
@@ -22,4 +22,22 @@ public class MyClass {
         // Calling the public method
         myObject.publicMethod();
     }
+	#feature202 by ethan in oct2024 on reports module
+	 public void display2(int a) {
+    System.out.println("Method with a single parameter: " + a);
+  }
+
+  public static void main(String[] args) {
+    
+    // create an object of Main
+    Main obj = new Main();
+
+    // calling method with no parameter
+    obj.display1();
+    
+    // calling method with the single parameter
+    obj.display2(24);
+  }
+}
+#end of feature202 code
 }
